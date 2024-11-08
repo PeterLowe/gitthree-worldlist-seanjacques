08d1d9723dbc25a372eb30df019262edc1cde5b4
(Student  C00312837) Sean O Neill <C00312837@setu.ie>
2024-11-08 10:18:30 +0000

    letter Y

--- PetesWorld/main.cpp
+++ PetesWorld/main.cpp
@@ -54,29 +54,30 @@ int main()
 		{
 			std::cout << "Australia, Antarctica, Aruba, Anguilla Leeward Islands, Antigua and Barbuda, American Samoa" << std::endl;
 		}
-		if (letter == 'y' || letter == 'Y')
+	}
+	if (letter == 'y' || letter == 'Y')
+	{
+		if (region == 1)
 		{
-			if (region == 1)
-			{
 				std::cout << "There are no Ys in the Americas" << std::endl;
-			}
-			if (region == 2)
-			{
-				std::cout << "There are no Ys in Europe" << std::endl;
-			}
-			if (region == 3)
-			{
-				std::cout << "There are no Ys in Africa" << std::endl;
-			}
-			if (region == 4)
-			{
-				std::cout << "There are no Ys in Asia" << std::endl;
-			}
-			if (region == 5)
-			{
-				std::cout << "Yemen" << std::endl;
-			}
 		}
+		if (region == 2)
+		{
+			std::cout << "There are no Ys in Europe" << std::endl;
+		}
+		if (region == 3)
+		{
+			std::cout << "There are no Ys in Africa" << std::endl;
+		}
+		if (region == 4)
+		{
+			std::cout << "There are no Ys in Asia" << std::endl;
+		}
+		if (region == 5)
+		{
+			std::cout << "Yemen" << std::endl;
+		}
+	
 	}	
 	if (letter == 'z' || letter == 'Z') // written by Pete
 	{
